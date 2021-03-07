e7fea13956f8ce9f8dbf14b673ea1a5bc59e9780
unknown <55632619+gonzagod@users.noreply.github.com>
2021-03-07 21:23:36 +0100

    Secene1 tilemap

--- Goonies/Goonies/TileMap.cpp
+++ Goonies/Goonies/TileMap.cpp
@@ -50,7 +50,8 @@ bool TileMap::loadLevel(const string &levelFile)
 	ifstream fin;
 	string line, tilesheetFile;
 	stringstream sstream;
-	char tile;
+	char row, column;
+	int irow, icolumn, coords;
 
 	fin.open(levelFile.c_str());
 	if (!fin.is_open())
@@ -82,15 +83,21 @@ bool TileMap::loadLevel(const string &levelFile)
 	{
 		for (int i = 0; i<mapSize.x; i++)
 		{
-			fin.get(tile);
-			if (tile == ' ')
+			fin.get(row);
+			irow = row - 65;
+			fin.get(column);
+			icolumn = column - 96;
+			coords = irow*tilesheetSize.x + icolumn;
+			map[j*mapSize.x + i] = coords;
+			/*if (tile == ' ')
 				map[j*mapSize.x + i] = 0;
 			else
 				map[j*mapSize.x + i] = tile - 96;
+				*/
 		}
-		fin.get(tile);
+		fin.get(row);
 #ifndef _WIN32
-		fin.get(tile);
+		fin.get(row);
 #endif
 	}
 	fin.close();
@@ -115,7 +122,7 @@ void TileMap::prepareArrays(const glm::vec2 &minCoords, ShaderProgram &program)
 				// Non-empty tile
 				nTiles++;
 				posTile = glm::vec2(minCoords.x + i * tileSize, minCoords.y + j * tileSize);
-				texCoordTile[0] = glm::vec2(float((tile - 1) % 2) / tilesheetSize.x, float((tile - 1) / 2) / tilesheetSize.y);
+				texCoordTile[0] = glm::vec2(float((tile - 1) % 16) / tilesheetSize.x, float((tile - 1) / 16) / tilesheetSize.y);
 				texCoordTile[1] = texCoordTile[0] + tileTexSize;
 				//texCoordTile[0] += halfTexel;
 				texCoordTile[1] -= halfTexel;
